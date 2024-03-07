0f6d33cafa66bc85f29eb1d102cf7cdc9310f356
efraim-paiva <efgaroto@gmail.com>
2024-03-07 18:38:30 -0300

    Add function to alter game status

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function alterarStatus(id) {
+    var game = document.getElementById(`game-${id}`);
+    var imagem = game.querySelector('.dashboard__item__image');
+    var buttom = game.querySelector('.dashboard__item__button');
+    var nome = game.querySelector('.dashboard__item__name');
+//    alert(`jogo ${nome.textContent} alterado para ${buttom.textContent}`);
+
+
+}
