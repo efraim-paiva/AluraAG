5e4b2232f24e488a31d27716e11b79e3a9c490bb
efraim-paiva <efgaroto@gmail.com>
2024-03-07 18:57:38 -0300

    Update button text and class in index.html, and fix typo in app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,18 @@ function alterarStatus(id) {
     var game = document.getElementById(`game-${id}`);
     var imagem = game.querySelector('.dashboard__item__image');
     var buttom = game.querySelector('.dashboard__item__button');
-    var nome = game.querySelector('.dashboard__item__name');
-//    alert(`jogo ${nome.textContent} alterado para ${buttom.textContent}`);
+    var name = game.querySelector('.dashboard__item__name');
+    //    alert(`jogo ${nome.textContent} alterado para ${buttom.textContent}`);
 
-
+    if (buttom.classList.contains('dashboard__item__button--return')) {
+        buttom.textContent = 'Alugar';
+        buttom.classList.remove('dashboard__item__button--return');
+        game.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented');
+        name.textContent = name.textContent.replace('(Alugado)', '');
+    } else {
+        buttom.textContent = 'Devolver';
+        buttom.classList.add('dashboard__item__button--return');
+        game.querySelector('.dashboard__item__img').classList.add('dashboard__item__img--rented');
+        name.textContent = name.textContent.replace('(Alugar)', '');
+    }
 }
