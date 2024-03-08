07cd072eebbc5442058eff685e4f514c8822e91d
efraim-paiva <efgaroto@gmail.com>
2024-03-07 21:42:23 -0300

    Refactor alterarStatus function to use more descriptive variable names

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 function alterarStatus(id) {
     var game = document.getElementById(`game-${id}`);
-    var imagem = game.querySelector('.dashboard__item__image');
-    var buttom = game.querySelector('.dashboard__item__button');
+    var imageElement = game.querySelector('.dashboard__item__img');
+    var buttonElement = game.querySelector('.dashboard__item__button');
 
-    if (buttom.classList.contains('dashboard__item__button--return')) {
+    if (buttonElement.classList.contains('dashboard__item__button--return')) {
         if (confirm('Tem certeza que deseja devolver o jogo?') == false) {
             return;
         }
-        buttom.classList.remove('dashboard__item__button--return');
-        buttom.textContent = 'Alugar';
-        game.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented');
+        buttonElement.classList.remove('dashboard__item__button--return');
+        buttonElement.textContent = 'Alugar';
+        imageElement.classList.remove('dashboard__item__img--rented');
     } else {
-        buttom.classList.add('dashboard__item__button--return');
-        buttom.textContent = 'Devolver';
-        game.querySelector('.dashboard__item__img').classList.add('dashboard__item__img--rented');
+        buttonElement.classList.add('dashboard__item__button--return');
+        buttonElement.textContent = 'Devolver';
+        imageElement.classList.add('dashboard__item__img--rented');
     } alugados();
 }
 
